608f9b6ba40c4be76873ae1231d334882176da18
ATIN AGNIHOTRI <atin.agnihotri@paytmmoney.com>
2021-05-09 22:39:17 +0530

    Initial Commit

---
+++ Splitsy/SplitsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitsyApp.swift
+//  Splitsy
+//
+//  Created by Atin Agnihotri on 09/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SplitsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
