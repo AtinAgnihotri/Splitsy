185cc03db6a2b33551f8000ac9a5368ef76e1992
Atin Agnihotri <atinagnihotri@gmail.com>
2021-05-16 00:14:14 +0530

    Adding Navigation Bar

--- Splitsy/ContentView.swift
+++ Splitsy/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    /*
+     some View. This means it will return something that conforms to the View protocol, but that extra some keyword adds an important restriction: it must always be the same kind of view being returned – you can’t sometimes return one type of thing and other times return a different type of thing.
+     */
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            // SwiftUI entertains upto 10 children per view
+            Form {
+                Section {
+                    Text("Hello, world!")
+                        .padding()
+                    Text("Hello, world!")
+                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+//                    Text("Hello, world!")
+//                        .padding()
+                }
+//                Section {
+//                Text("Hello, world!")
+//                    .padding()
+//                Text("Hello, world!")
+//                    .padding()
+//                Text("Hello, world!")
+//                    .padding()
+//
+//                }
+            }
+//            .navigationBarTitle(Text("SwiftUI"), displayMode: .inline)
+            // Modifiers are regular methods with one small difference: they always return a new instance of whatever you use them on.
+            .navigationBarTitle("Splitsy")
+        }
+        
+        
     }
 }
 
