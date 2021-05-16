0ff7d9a80e069de1ed6f80ffe5a4a7bdd94cdaed
Atin Agnihotri <atinagnihotri@gmail.com>
2021-05-16 20:27:03 +0530

    Extracting AmountView

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Splitsy
 
-App for splitting the bill, even accounting for the tip. Made in MVC design pattern in SwiftUI.
+App for splitting the bill, even accounting for the tip. Made in SwiftUI
--- Splitsy/ContentView.swift
+++ Splitsy/ContentView.swift
@@ -9,20 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    @State private var totalAmount: Double = 0
     @State private var totalAmountText: String = ""
     @State private var count: Int = 0
     @State private var tipCount: Int = 0
-    @State private var split: Double = 0
+    
     let tipAmount = [0, 5, 10, 15, 20, 25]
+    
+    var totalAmountAfterTip: Double {
+        guard let totalAmount = Double(totalAmountText) else {
+            return 0
+        }
+        return totalAmount * (1 + Double(tipAmount[tipCount])/100)
+    }
+    
+    var split: Double {
+        guard totalAmountAfterTip != 0 else {
+            return 0
+        }
+        return totalAmountAfterTip / (Double(count) + 2) // As 2 People is at index 0 of the
+    }
+    
+    
     /*
      some View. This means it will return something that conforms to the View protocol, but that extra some keyword adds an important restriction: it must always be the same kind of view being returned – you can’t sometimes return one type of thing and other times return a different type of thing.
      */
     var body: some View {
         NavigationView {
             Form {
+                // Total Amount and Number of People
                 Section {
-                    TextField("Enter total amount", text: $totalAmountText)
+                    TextField("Amount", text: $totalAmountText)
                         .keyboardType(.decimalPad)
                     Picker("Number of People", selection: $count) {
                         ForEach(2..<100) {
@@ -30,37 +46,47 @@ struct ContentView: View {
                         }
                     }
                 }
-                Section {
+                
+                // Tip Percentage
+                Section(header: Text("How much tip do you want to leave")
+                                    .textCase(nil)){
+                    
                     Picker ("Select Tip Amount", selection: $tipCount){
                         ForEach (0 ..< tipAmount.count) {
                             Text("\(tipAmount[$0])%")
                         }
                     }.pickerStyle(SegmentedPickerStyle())
                 }
-                Section {
-                    Button("Split!") {
-                        calcuateSplit()
-                    }
-                }
-                Section {
-                    Text(String(format: "Your split is %.2f", split))
-                }
+                
+                // Total Amount
+                AmountView(headerText: "Total amount after tip", amount: totalAmountAfterTip)
+                
+                // Split Amount
+                AmountView(headerText: "Amount per person", amount: split)
+
             }
             .navigationBarTitle("Splitsy")
         }
         
     }
     
-    func calcuateSplit() {
-        guard let totalAmount = Double(totalAmountText) else {
-            totalAmountText = ""
-            split = 0
-            return
+}
+
+// A custom view to display amount in Forms
+struct AmountView: View {
+    var headerText: String
+    var amount: Double
+    
+    init(headerText: String, amount: Double) {
+        self.headerText = headerText
+        self.amount = amount
+    }
+    
+    var body: some View {
+        Section (header: Text(headerText)
+                            .textCase(nil)){
+            Text("\(amount, specifier: "%.2f")")
         }
-        let totalTippedAmount = totalAmount * ( 1 + (Double(tipAmount[tipCount]) / 100.0))
-        split = totalTippedAmount / Double(count + 2)
-        print("totalTippedAmount: \(totalTippedAmount), split: \(split)")
-        
     }
 }
 
